d752d4402cda2dbcb943211d1bd9c93b2ab1c6aa
MobbyGFX96 <MobbyGFX96@hotmail.co.uk>
2013-03-23 19:44:47 +0000

    heh

--- Slick2D/src/net/solace/GameController.java
+++ Slick2D/src/net/solace/GameController.java
@@ -1,5 +1,7 @@
 package net.solace;
 
+import net.solace.menu.MenuState;
+
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
@@ -11,6 +13,7 @@ public class GameController extends StateBasedGame {
 	}
 
 	public void initStatesList(GameContainer container) throws SlickException {
+		addState(new MenuState());
 		addState(new GameState());
 	}
 
--- Slick2D/src/net/solace/menu/MenuState.java
+++ Slick2D/src/net/solace/menu/MenuState.java
@@ -1,7 +1,4 @@
-package net.solace;
-
-import net.solace.entity.Alien;
-import net.solace.entity.Player;
+package net.solace.menu;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
@@ -9,31 +6,25 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-public class GameState extends BasicGameState {
+public class MenuState extends BasicGameState {
 
-	public Player player;
-	public Alien alien;
-	
 	public void init(GameContainer container, StateBasedGame game)
 			throws SlickException {
-		player = new Player(400, 300);
-		alien = new Alien(500, 400);
+
 	}
 
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
-		player.render(container, g);
-		alien.render(container, g);
+
 	}
 
 	public void update(GameContainer container, StateBasedGame game, int deltaMS)
 			throws SlickException {
-		player.update(container, deltaMS);
-		alien.update(container, deltaMS);
+
 	}
 
 	public int getID() {
-		return 1;
+		return 0;
 	}
 
 }
