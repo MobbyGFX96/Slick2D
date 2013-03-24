c3bad189c4c8bcf8a4571c7681d1672a260f4e81
MobbyGFX96 <MobbyGFX96@hotmail.co.uk>
2013-03-24 02:26:26 +0000

    lel

--- Slick2D/src/net/solace/Main.java
+++ Slick2D/src/net/solace/Main.java
@@ -8,6 +8,7 @@ public class Main {
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new GameController());
 		app.setDisplayMode(800, 600, false);
+		app.setShowFPS(false);
 		app.start();
 	}
 
